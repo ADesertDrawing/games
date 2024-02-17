efc62051fb7a775a9b8d4e1e80985a200a9e775c
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2024-02-17 11:13:27 +0000

    Beautiful gradient from red to blue. Beautiful.

--- A_beautiful_sunset/js/script.js
+++ A_beautiful_sunset/js/script.js
@@ -3,11 +3,37 @@ A Beautiful Sunset
 by A Desert Drawing
 */
 "use strict";
-function setup() {
-    createCanvas(500, 500);
 
+let topR;
+let topG;
+let topB;
+
+let bottomR;
+let bottomG;
+let bottomB;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    noSmooth();
+
+    topR = 255;
+    topG = 0;
+    topB = 0;
+
+    bottomR = 0;
+    bottomG = 0;
+    bottomB = 255;
 }
 
 function draw() {
-    background(0, 0, 0);
+    background(220);
+
+    const topColor = color(topR, topG, topB);
+    const bottomColor = color(bottomR, bottomG, bottomB);
+
+    for (let y = 0; y < height; y++) {
+        const lineColor = lerpColor(topColor, bottomColor, y / height);
+        stroke(lineColor);
+        line(0, y, width, y);
+    }
 }
