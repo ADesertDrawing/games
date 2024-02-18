7d3433b8c07655b4e77439bec1d25e1a3e3e25b0
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2024-02-18 17:28:17 +0000

    4 states working but no text on day....?

--- A_beautiful_sunset/js/script.js
+++ A_beautiful_sunset/js/script.js
@@ -4,6 +4,8 @@ by A Desert Drawing
 */
 "use strict";
 
+let state = `day`;
+
 let circle = {
     x: 250,
     y: -100,
@@ -23,24 +25,50 @@ let sky = {
     r: 50,
     g: 100,
     b: 200,
-    transparency: 255
+    //transparency: 255
 }
+
 
 function setup() {
     createCanvas(500, 500);
+    setInterval(changeState, 5000);
 
 }
 
 function draw() {
     background(sky.r, sky.g, sky.b);
-
-    sunset();
-
-
+    if (state === `day`) {
+        day();
+        console.log(`day`);
+    }
+    else if (state === `sunset`) {
+        sunset();
+        console.log(`sunset`);
+    }
+    else if (state === `night`) {
+        night();
+        console.log(`night`);
+    }
+    else if (state === `sunrise`) {
+        sunrise();
+        console.log(`sunrise`);
+    }
 }
 
+function day() {
+    sky.r = 50, sky.g = 100, sky.b = 200;
+    background(50, 100, 200);
+    //  push();
+
+    fill(255);
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text(`Ah, you missed the sunrise`, width / 2, height / 2);
+    // pop();
+}
 
 function sunset() {
+
     push();
     circle.x = circle.x + circle.vx;
     circle.y = circle.y + circle.vy;
@@ -48,18 +76,56 @@ function sunset() {
     fill(circle.r, circle.g, circle.b);
     ellipse(circle.x, circle.y, circle.size);
     //Turn sun from yellow to red as it falls
-    circle.g = circle.g - 0.3;
+    circle.g = circle.g - 0.4;
     pop();
 
     push();
     noStroke();
-
-    // fill(sky.r, sky.g, sky.b, sky.transparency);
-    // rect(sky.x, sky.y, sky.width, sky.height);
-    // sky.transparency = sky.transparency - 1;
-    sky.r = sky.r - 0.02;
-    sky.g = sky.g - 0.3;
-    sky.b = sky.b - 0.3;
+    sky.r = sky.r - 0.01;
+    sky.g = sky.g - 0.4;
+    sky.b = sky.b - 0.4;
     pop();
 }
 
+function night() {
+    push();
+    background(0);
+
+    fill(255);
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text(`Ah, you missed the sunset`, width / 2, height / 2);
+    pop();
+}
+
+function sunrise() {
+    push();
+    background(0);
+
+    fill(255);
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text(`Sunrise goes here`, width / 2, height / 2);
+    pop();
+}
+
+function changeState() {
+    if (state === `day`) {
+        sky.r = 50, sky.g = 100, sky.b = 200;
+        circle.x = 250, circle.y = -100, circle.size = 200, circle.vx = 0, circle.vy = 1, circle.r = 200, circle.g = 200, circle.b = 0;
+
+        state = `sunset`;
+    }
+    else if (state === `sunset`) {
+        state = `night`;
+        sky.r = 0, sky.g = 0, sky.b = 0;
+    }
+    else if (state === `night`) {
+        sky.r = 0, sky.g = 0, sky.b = 0;
+        state = `sunrise`;
+    }
+    else if (state === `sunrise`) {
+        sky.r = 50, sky.g = 100, sky.b = 200;
+        state = `day`;
+    }
+}
