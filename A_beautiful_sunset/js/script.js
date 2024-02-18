1985674ace326f4f66e214e29e267a909ad6aa6a
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2024-02-18 15:37:19 +0000

    Sky goes black, sun goes red and down.

--- A_beautiful_sunset/js/script.js
+++ A_beautiful_sunset/js/script.js
@@ -10,10 +10,21 @@ let circle = {
     size: 200,
     vx: 0,
     vy: 1,
-    r: 255,
-    g: 255,
+    r: 200,
+    g: 200,
     b: 0
 };
+
+let sky = {
+    x: 0,
+    y: 0,
+    width: 500,
+    height: 500,
+    r: 50,
+    g: 100,
+    b: 200,
+    transparency: 255
+}
 
 function setup() {
     createCanvas(500, 500);
@@ -21,7 +32,16 @@ function setup() {
 }
 
 function draw() {
-    background(50, 100, 200);
+    background(sky.r, sky.g, sky.b);
+
+    sunset();
+
+
+}
+
+
+function sunset() {
+    push();
     circle.x = circle.x + circle.vx;
     circle.y = circle.y + circle.vy;
     noStroke();
@@ -29,7 +49,17 @@ function draw() {
     ellipse(circle.x, circle.y, circle.size);
     //Turn sun from yellow to red as it falls
     circle.g = circle.g - 0.3;
+    pop();
 
+    push();
+    noStroke();
+
+    // fill(sky.r, sky.g, sky.b, sky.transparency);
+    // rect(sky.x, sky.y, sky.width, sky.height);
+    // sky.transparency = sky.transparency - 1;
+    sky.r = sky.r - 0.02;
+    sky.g = sky.g - 0.3;
+    sky.b = sky.b - 0.3;
+    pop();
 }
-
 
