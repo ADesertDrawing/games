e03922659f44bccaa7b246bc1a1f8786cbf46675
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2024-04-02 15:39:17 +0100

    Setting up scenes

--- Fruitflies/js/main.js
+++ Fruitflies/js/main.js
@@ -4,6 +4,18 @@ by A Desert Drawing
 */
 "use strict";
 
+let config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: `arcade`
+    },
+    scene: [Play]
+};
+
+let game = new Phaser.Game(config);
+
 //Setting up all the player movement images
 let leftLeft;
 let leftBack;
