c0f51cc826b3086974abca50025c4961cf168cca
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2024-09-20 18:23:32 +0100

    Added changeDirection and tidying up a bit

--- Fruitflies/js/Person.js
+++ Fruitflies/js/Person.js
@@ -13,25 +13,35 @@ class Person extends Phaser.Physics.Arcade.Sprite {
         // this.setBounce(0.5, 0.5);
         // this.setDrag(-10, -10);
         // this.setDepth(this.y);
-        // let velocities = [0, 25, 50, 75, 100, 125, 150, 175, 200];
+        this.setVelocity(100, -100);
 
 
     }
 
 
     create() {
+
         super.create();
 
-        // let randomXVelocity = random(velocities);
-        // let randomYVelocity = random(velocities);
+        this.changeDirection();
 
+    }
+
+    changeDirection() {
+        // Multiple x and y by either -1, 0, or 1 to change direction (or possibly not)
+        this.velocity.x *= Phaser.Utils.Array.GetRandom([-1, 0, 1]);
+        this.velocity.y *= Phaser.Utils.Array.GetRandom([-1, 0, 1]);
+        // Wait 2 seconds then do it again
+        this.scene.time.addEvent({
+            delay: Math.random() * 2000, // Wait two seconds
+            callback: changeDirection,
+            callbackContext: this
+        });
     }
 
     update() {
 
-        this.setVelocity(this.randomXVelocity, this.randomYVelocity);
-
-        this.person.depth = this.person.y;
+        // this.person.depth = this.person.y;
     }
 
 
